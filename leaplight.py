58c81b409989550c0b0a725a42c33253c5988a96
jeremy <jere.chua@gmail.com>
2016-08-20 15:34:41 -0700

    Finished doing python hello world sample

--- leaplight.py
+++ leaplight.py
@@ -1,1 +1,30 @@
+import sys
 from lib import Leap
+
+class Listener(Leap.Listener):
+    def on_connect(self, controller):
+        print "Connected"
+
+    def on_frame(self, controller):
+        # print "Frame available"
+        frame = controller.frame()
+
+        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
+          frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
+        pass
+
+def main():
+
+    listener = Listener()
+    controller = Leap.Controller()
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
