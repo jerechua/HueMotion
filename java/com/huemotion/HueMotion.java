ca67aaddf4a5730af79efcb8b3cbd19bb9ff9442
jeremy <jere.chua@gmail.com>
2016-08-21 01:37:08 -0700

    Move all logic to HueMotion.java

---
+++ java/com/huemotion/HueMotion.java
@@ -0,0 +1,20 @@
+package com.huemotion;
+
+import com.huey.HueController;
+import com.huemotion.listener.MainListener;
+import com.leapmotion.leap.Controller;
+
+final class HueMotion {
+
+  private static final String APP_NAME = "HueMotion";
+
+  HueMotion() {
+    HueController hueController = new HueController(APP_NAME);
+    hueController.getAllLights();
+
+    MainListener listener = new MainListener();
+
+    Controller leapController = new Controller();
+    leapController.addListener(listener);
+  }
+}
--- java/com/huemotion/HueMotionMain.java
+++ java/com/huemotion/HueMotionMain.java
@@ -1,25 +1,14 @@
 package com.huemotion;
 
-import com.huey.HueController;
-import com.huemotion.listener.MainListener;
-import com.leapmotion.leap.Controller;
 import java.io.IOException;
 
 public final class HueMotionMain {
 
-  private static final String APP_NAME = "HueMotion";
-
   public static void main(String[] args) {
+
+    new HueMotion();
+
     System.out.println("Press Enter to quit...");
-
-    HueController hueController = new HueController(APP_NAME);
-    hueController.getAllLights();
-
-    MainListener listener = new MainListener();
-
-    Controller leapController = new Controller();
-    leapController.addListener(listener);
-
     try {
       System.in.read();
     } catch (IOException e) {
