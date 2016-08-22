506b098c737ed8fe84da069bdd42d9f7e9aa16f8
jeremy <jere.chua@gmail.com>
2016-08-21 18:59:24 -0700

    Add Google Guava libraries

---
+++ README.md
@@ -0,0 +1,10 @@
+HueMotion
+=========
+
+Dependencies
+------------
+These are all the dependencies the project needs.
+
+- LeapMotion Orion SDK (TODO)
+- Philips Hue SDK (TODO)
+- [Google Guava core libraries (release 19)](https://github.com/google/guava/wiki/Release19)
--- run_java.sh
+++ run_java.sh
@@ -1,9 +1,9 @@
 cd java
 javac \
-  -classpath "../lib/LeapJava.jar;../lib/huelocalsdk.jar;../lib/huesdkresources.jar" \
+  -classpath "../lib/LeapJava.jar;../lib/huelocalsdk.jar;../lib/huesdkresources.jar;../lib/guava-19.0.jar" \
   -sourcepath . \
   "com/huemotion/HueMotionMain.java" && \
 java \
-  -classpath ".;../lib/LeapJava.jar;../lib/huelocalsdk.jar;../lib/huesdkresources.jar" \
+  -classpath ".;../lib/LeapJava.jar;../lib/huelocalsdk.jar;../lib/huesdkresources.jar;../lib/guava-19.0.jar" \
   -Djava.library.path="../lib/x64" \
   "com.huemotion.HueMotionMain"
